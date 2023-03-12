cef51675d381a52016894700784e302fbf8e49b0
pilotso11 <seth@oshers.com>
2023-03-12 16:00:26 +0000

    Added MIT LICENSE

--- examples/trivial-gorm/main.go
+++ examples/trivial-gorm/main.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2023 Seth Osher
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package main
 
 import (
@@ -10,6 +32,14 @@ import (
 	"gorm.io/gorm"
 )
 
+// This example exposes a CRUD API for "Employee" backed by GORM on http://127.0.0.1:8080/api/v1/employees
+// Using the Employee type as both the data object and as the transport object.
+// With GET employees/ to get all
+// With GET employees/:id to get one
+// With PUT employees/:id to change one
+// With DELETE employees/:id to delete one
+// With POST employees/ to create a new one
+
 type Employee struct {
 	gorm.Model
 	Name       string
@@ -17,13 +47,6 @@ type Employee struct {
 	Department string
 }
 
-// Exposes a CRUD API for "Employee" backed by GORM on http://127.0.0.1:8080/api/v1/employees
-// Using the Employee type as both the data object and as the transport object.
-// With GET employees/ to get all
-// With GET employees/:id to get one
-// With PUT employees/:id to change one
-// With DELETE employees/:id to delete one
-// With POST employees/ to create a new one
 func main() {
 	app := fiber.New()
 	dbUrl := "sqlite:test.db"
